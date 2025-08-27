8c77c95bbba508fceea01a0581c2dc4211d4eb31
Gingersmurlf <theodorenghoff@gmail.com>
2025-08-27 10:25:20 +0200

    opgave 2 færdig

--- script.js
+++ script.js
@@ -15,3 +15,28 @@ console.log(person["hjemby"]);
 
 console.log("Opgave 1 færdig");
 
+// Opgave 2
+
+let beastmode = {
+    fornavn: "Mikkel",
+    efternavn: "Taco",
+    alder: 17,
+    hjemby: "Kundby",
+    fødselsdag: new Date("2007-02-19"),
+    sigma: false,
+    hobbier: ["stjæle", "looksmaxing", "cs2"],
+    kæledyr: {
+        type: "hund",
+        name: "vovse"
+    }
+}
+
+console.log(beastmode.kæledyr.type);
+beastmode.hobbier.forEach(function(hobby){
+    console.log(hobby)
+});
+
+console.log("Opgave 2 færdig");
+
+
+// Opgave 3
