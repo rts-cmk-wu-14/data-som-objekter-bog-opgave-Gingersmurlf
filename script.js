c569cce055eb3a0cc3a53fe639e130463982d87b
Gingersmurlf <theodorenghoff@gmail.com>
2025-08-27 10:07:06 +0200

    opgave 1 færdig

---
+++ script.js
@@ -0,0 +1,17 @@
+let person = {
+    fornavn: "Mikkel",
+    efternavn: "Taco",
+    alder: 17,
+    hjemby: "Kundby",
+    fødselsdag: new Date("2007-02-19")
+}
+
+console.log(person.fornavn);
+console.log(person.efternavn);
+
+console.log(person.fødselsdag);
+
+console.log(person["hjemby"]);
+
+console.log("Opgave 1 færdig");
+
